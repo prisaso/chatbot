5a4b75799a8b3dc24fd1947c14d31e9952bde26b
prisaso <131245470+prisaso@users.noreply.github.com>
2024-09-05 09:18:12 +0000

    updated now working

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,14 +44,15 @@ dashboard_information = """
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = ""
+openai_api_key = st.secrets["api_key"]
+
+
 if not openai_api_key:
-#     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-# else:
+     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+else:
 
 #     # Create an OpenAI client.
-    # OpenAI.api_key = "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"
-    client = OpenAI(api_key="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")
+    client = OpenAI(api_key=openai_api_key)
 
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
@@ -80,7 +81,7 @@ if not openai_api_key:
 
         # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="gpt-4",
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
